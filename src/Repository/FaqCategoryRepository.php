4b22d18cbc9ea7874941ae70fba7dffd054a1d36
FloFlo-L <florian.lescribaa@gmail.com>
2026-02-25 08:13:50 +0100

    Add the classes FaqCategoryRepository and FaqRepository (empty for now)

---
+++ src/Repository/FaqCategoryRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Module\Faq\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class FaqCategoryRepository extends EntityRepository
+{
+}
---
+++ src/Repository/FaqRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Module\Faq\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class FaqRepository extends EntityRepository
+{
+}
